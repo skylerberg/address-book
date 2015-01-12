fb046bfb22bee2910d41c20484046eb2843470d5
Skyler Berg <skylertheberg@gmail.com>
2015-01-11 19:37:58 -0800

    Create setup script for project

---
+++ address/__init__.py
@@ -0,0 +1,5 @@
+import main
+from book import Book
+from entry import Entry
+
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from os.path import join, dirname
+import address
+
+setup(
+    name='address',
+    version=address.__version__,
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    entry_points={
+        'console_scripts': ['address = address.main:main']
+        },
+    )
