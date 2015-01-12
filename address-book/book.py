a9d3c8a9fe60d257f82123b9e9abd83d6f9ae7c3
Skyler Berg <skylertheberg@gmail.com>
2015-01-11 18:26:04 -0800

    Make style changes to be closer to following PEP8

--- address-book/book.py
+++ address-book/book.py
@@ -1,9 +1,8 @@
-
 import json
 
 # global constants
 # make it easier to use the name of the attributes of Entry
-# and the key name in json file(those strings need to follow 
+# and the key name in json file(those strings need to follow
 # variable naming conventions,e.g. no space in between)
 LNAME = "lname"
 FNAME = "fname"
@@ -21,7 +20,7 @@ class BookEncoder(json.JSONEncoder):
     '''
     def default(self, obj):
         if isinstance(obj, Book):
-            return {"entries":[entry.__dict__ for entry in obj.entries]}
+            return {"entries": [entry.__dict__ for entry in obj.entries]}
         return json.JSONEncoder.default(self, obj)
 
 
@@ -29,12 +28,12 @@ class Book:
     '''
     First implementation of an address book with very simple functionality.
     '''
-    def __init__(self, path = None): #a distinction between new book and opened book
+    def __init__(self, path=None):  # a distinction between new book and opened book
         '''
         :arg path: Path to the file to be opened.
         :type path: String
         '''
-        #self.num_fields  may be needed for supporting customized fileds
+        # self.num_fields  may be needed for supporting customized fileds
         self.entries = []
         if path is not None:
             f = open(path)
@@ -48,37 +47,46 @@ class Book:
                 zip_code = entry[ZIP_CODE]
                 phone_num = entry[PHONE_NUM]
                 email = entry[EMAIL]
-                self.entries.append(Entry(fname,lname,addr,city,state,zip_code,phone_num,email))
+                self.entries.append(Entry(fname,
+                                          lname,
+                                          addr,
+                                          city,
+                                          state,
+                                          zip_code,
+                                          phone_num,
+                                          email))
             f.close()
-            
-    def add_entry(self,entry):
+
+    def add_entry(self, entry):
         '''
         Add an entry to the book
 
-        :arg entry: An object of Entry class, representing an entry in an address book
+        :arg entry: An object of Entry class, representing an entry in an
+          address book
         :type entry: Entry
         '''
         self.entries.append(entry)
-    
+
     def show_entry(self):
         '''
         Print all the entries in the book
         '''
-        for index,entry in enumerate(self.entries):
-            print index,entry
+        for index, entry in enumerate(self.entries):
+            print index, entry
 
-    def save_as(self,path):
+    def save_as(self, path):
         '''
         Save the book into a file using json format
 
-        :arg path: The path to the file, if the file exists, it will be overwritten
+        :arg path: The path to the file, if the file exists, it will be
+          overwritten
         :type path: String
         '''
-        f = open(path,"w")
-        json.dump(self,f,cls=BookEncoder)
+        f = open(path, "w")
+        json.dump(self, f, cls=BookEncoder)
         f.close()
 
-    def sort(self,attr):
+    def sort(self, attr):
         '''
         Sort the entries based on one of the attributes
 
@@ -87,20 +95,20 @@ class Book:
         '''
 
         if attr == LNAME:
-            self.entries = sorted(self.entries,key=lambda entry:entry.__dict__[LNAME])
+            self.entries = sorted(self.entries, key=lambda entry: entry.__dict__[LNAME])
         elif attr == FNAME:
-            self.entries = sorted(self.entries,key=lambda entry:entry.__dict__[FNAME])
+            self.entries = sorted(self.entries, key=lambda entry: entry.__dict__[FNAME])
         elif attr == ZIP_CODE:
-            self.entries = sorted(self.entries,key=lambda entry:entry.__dict__[ZIP_CODE])
+            self.entries = sorted(self.entries, key=lambda entry: entry.__dict__[ZIP_CODE])
         else:
             print "Unimplemented!"
 
-    def delete_entry(self,index):
+    def delete_entry(self, index):
         '''
         :arg index: The index of the entry to be deleted
         :type index: Int
         '''
-        entry = self.entries.pop(index) #save the deleted entry in case the user wants to withdraw
+        entry = self.entries.pop(index)  # save the deleted entry in case the user wants to withdraw
 
 
 class Entry:
@@ -134,7 +142,7 @@ def main():
                \n1-Creat a new book\
                \n2-Open a book\
                \n9-quit"
-              
+
         inp = raw_input().strip()
         if not inp.isdigit():
             print "Invalid input!"
@@ -155,7 +163,7 @@ def main():
                     \n4-sort by(fname, lname or zip_code)\
                     \n5-delete an entry(by index based on print)\
                     \n9-return to up level"
-                    
+
                 inp2 = raw_input().strip()
                 if not inp2.isdigit():
                     print "Invalid input!"
@@ -165,19 +173,19 @@ def main():
                     fname = raw_input("firstname:")
                     lname = raw_input("lastname:")
                     addr = raw_input("addr:")
-                    city =raw_input("city:")
+                    city = raw_input("city:")
                     state = raw_input("state:")
                     zip_code = raw_input("zip:")
-                    phone_num =raw_input("phone number:")
-                    email =raw_input("email:")
-                    b.add_entry(Entry(fname,lname,addr,city,state,zip_code,phone_num,email))
+                    phone_num = raw_input("phone number:")
+                    email = raw_input("email:")
+                    b.add_entry(Entry(fname, lname, addr, city, state, zip_code, phone_num, email))
                 elif choice2 == 2:
                     b.show_entry()
                 elif choice2 == 3:
                     path = raw_input("file name:")
                     b.save_as(path)
                 elif choice2 == 4:
-                    attr = raw_input(("input '%s', '%s' or '%s':") % (FNAME,LNAME,ZIP_CODE))
+                    attr = raw_input(("input '%s', '%s' or '%s':") % (FNAME, LNAME, ZIP_CODE))
                     b.sort(attr)
                 elif choice2 == 5:
                     l = len(b.entries)
@@ -185,7 +193,7 @@ def main():
                         print "Nothing left!"
                         continue
                     index = raw_input("input 0-"+str(l-1)+":")
-                    if not index.isdigit() or int(index)<0 or int(index) > l-1:
+                    if not index.isdigit() or int(index) < 0 or int(index) > l-1:
                         print "Invalid input!"
                         continue
                     b.delete_entry(int(index))
