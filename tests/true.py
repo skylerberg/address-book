90e1841ab7b59126e5e44d7e9e7ec1799579843e
Skyler Berg <skylertheberg@gmail.com>
2015-01-14 16:15:12 -0800

    Add files for travis and coveralls

---
+++ tests/true.py
@@ -0,0 +1,17 @@
+import mock
+import unittest
+
+class TestTest(unittest.TestCase):
+    """
+    This class is meant to make sure that travis and coveralls are working
+    properly.
+    """
+
+    def setUp(self):
+        pass
+
+    def test_true(self):
+        pass
+
+    def tearDown(self):
+        pass
