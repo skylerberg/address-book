0d532d421c54c7c900670ee1d6d8025840d2f128
Skyler Berg <skylertheberg@gmail.com>
2015-01-19 22:15:37 -0800

    Fix typos in docstring for validate

--- address/utility.py
+++ address/utility.py
@@ -9,7 +9,7 @@ from constants import *
 
 def validate(attr, string):  # return value may need to be improved
     '''
-    Validate if a string matches the format of a specific attribute
+    Validate if a string matches the format of a specific attribute.
 
     :arg attr: The specific attribute
     :arg string: The string to be validated
@@ -18,7 +18,7 @@ def validate(attr, string):  # return value may need to be improved
 
     :returns: A corresponding MatchObject instance or True if the string
       matches. None if it does not match.
-    :rtype: Mathcobject or None
+    :rtype: MatchObject, True, or None
     '''
     if attr == EMAIL:
         return re.match(r"^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.\w+$", string)
