ab8305f082691c7c007902b31623f7112b8277e0
Skyler Berg <skylertheberg@gmail.com>
2015-01-19 21:24:54 -0800

    Update setup.py to reflect recent change to README's name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import address
 setup(
     name='address',
     version=address.__version__,
-    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
     entry_points={
         'console_scripts': ['address = address.main:main']
         },
