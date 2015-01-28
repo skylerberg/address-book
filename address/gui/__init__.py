04220bfa9fa05120864b2912796bcf67fdd3ad24
Skyler Berg <skylertheberg@gmail.com>
2015-01-27 22:35:00 -0800

    Clean up the gui modules __init__.py

--- address/gui/__init__.py
+++ address/gui/__init__.py
@@ -3,13 +3,8 @@ The GUI module contains submodules for each type of window as well as a main
 function to start the application.
 """
 import Tkinter as tk
-import os
 
-from address.constants import main_tk_root
 from address.gui.startwindow import StartWindow
-from address.gui.mainwindow import MainWindow
-from address import utility
-
 
 
 def main():
@@ -18,17 +13,8 @@ def main():
     """
     root = tk.Tk()
     root.geometry("500x450+300+300")
-    root.title("Team 2 Address Book")
-    main_tk_root[1] = root
-    main_tk_root[1].update()
-
-    data_dir = os.path.expanduser("~")+"/.address"
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-    os.chdir(data_dir)
-
-    start_window=StartWindow(main_tk_root[1])
-
+    root.title("Address Book")
+    StartWindow(root)
     tk.mainloop()
 
 
