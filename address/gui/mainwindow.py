734a9a116787f9580d91be0fd344f4dc5ba75d5c
Skyler Berg <skylertheberg@gmail.com>
2015-01-29 14:45:36 -0800

    Remove old comments

--- address/gui/mainwindow.py
+++ address/gui/mainwindow.py
@@ -20,17 +20,14 @@ class MainWindow(object):
     user to perform basic CRUD operations.
     """
 
-#
-    def __init__(self, name, action, import_path=None):#since import_path is likely to be different from name..
+    def __init__(self, name, action, import_path=None):
         if action == NEW:
             self.book = book.Book()
-            #print 'here',name,action
         elif action == IMPORT:
             self.book = book.Book()
             self.book.import_from(import_path)
         elif action == OPEN:
             self.book = data.load(name)
-#
         self.parent = tk.Tk()
         self.parent.geometry("1500x1250+300+300")
         self.parent.title("Address Book")
@@ -40,12 +37,10 @@ class MainWindow(object):
         self.e2 = ""
         self.elist=[]
         print name
-        self.address=self.book.get_str_entries() #["name1, address1, phone1","name2, address2, phone2"]
-        #self.top = Toplevel(self.parent)
+        self.address=self.book.get_str_entries()
         self._menu = tk.Menu(self.parent, name='menu')
         self.build_submenus()
         self.top.config(menu=self._menu)
-        #self.top.grab_set()
         self.show()
 
     def show(self):
@@ -121,10 +116,6 @@ class MainWindow(object):
         dialogs.OpenDialog(self.parent)
 
     def newl(self):
-        """
-        creates a tab to place file name
-        calls okay button
-        """
         dialogs.NewDialog(self.parent)
 
     def mergel(self):
@@ -168,15 +159,9 @@ class MainWindow(object):
         data.save(self.name, self.book)
 
     def importl(self):
-        """
-        creates a tab to place file path
-        need to fix to have self.path, different from
-        file name for import
-        """
         dialogs.ImportDialog(self.parent)
 
     def exportl(self):
-        #self.top = tk.Toplevel(self.parent)
         self.export_path = tkFileDialog.asksaveasfilename(
                                     defaultextension=".tsv",
                                     filetypes=[("tab separate value","*.tsv")],
@@ -184,21 +169,8 @@ class MainWindow(object):
                                     parent=self.parent
                                     ) 
         if self.export_path:
-            self.exportfile()
-        #tk.Label(self.top, text="File Path").pack(padx=20, pady=10)
-        #self.e = tk.Entry(self.top)
-
-        #self.e.pack(padx=5)
-        #tk.Label(self.top, text="FileName").pack(padx=20, pady=10)
-        #self.e2 = tk.Entry(self.top)
-        #self.e2.pack(padx=5)
-        #b = tk.Button(self.top, text="okay", command=self.exportfile)
-        #b.pack(pady=5)
-
-    def exportfile(self):
-        print "exporting"
-        self.book.export_to(range(len(self.book)),self.export_path)
-        #self.top.destroy()
+            print "exporting"
+            self.book.export_to(range(len(self.book)),self.export_path)
 
     def addE(self):
         print "add"
@@ -230,40 +202,30 @@ class MainWindow(object):
     def deleteE(self):
         first_index = self.list_box.curselection()[0]
         value = self.address[int(first_index)]
-#
         entry_to_delete = self.to_entry(value)
         self.book.delete_entry(self.book.get_entry_index(entry_to_delete))
         self.address = self.book.get_str_entries()
         self.update_list()
-#
         print value
-        #self.address.pop(int(first_index))
-        #self.deleteShow()
-        #self.show()
-        #self.list_box.delete(int(first_index))
         print self.address
 
     def editE(self):
         
         first_index = self.list_box.curselection()[0]
         print 'index',first_index
-        #print self.address[first_index]
         self.value = self.address[int(first_index)]
         self.top = tk.Toplevel(self.parent)
         self.elist= []
         for i in range(len(self.book.get_fields())):
             tk.Label(self.top, text=self.book.get_fields()[i]).pack(padx=20, pady=10)
             self.elist.append(tk.Entry(self.top))
-##
             self.elist[i].insert(0, self.book.get_fields()[i])
             self.elist[i].pack(padx=5)
-        #print self.elist
         self.index = int(first_index)
         b = tk.Button(self.top, text="okay", command=self.getEditEntry)
         b.pack(pady=5)
 
     def getEditEntry(self):
-#
         var = []
         for i in range(len(self.book.get_fields())):
             var.append(self.elist[i].get())
@@ -276,7 +238,6 @@ class MainWindow(object):
             self.book.add_entry(entry_new)
             self.address = self.book.get_str_entries()
             self.list_box.delete(int(self.index))
-            #self.list_box.insert(tk.END, self.address[self.index])
             self.list_box.insert(tk.END, str(entry_new))
             self.address.sort()
             self.update_list()
@@ -284,7 +245,6 @@ class MainWindow(object):
             print self.address[self.index]
         else:
             mb.message(mb.WARNING,"Entry already exists!",parent=self.top)
-#
 
     def searchE(self):
         self.top = tk.Toplevel(self.parent)
@@ -308,17 +268,10 @@ class MainWindow(object):
         sub_book.set_entries(matches)
         self.address = sub_book.get_str_entries()
         self.update_list()
-#e  how to updating existing values
-        # do the search of book here
-        # as we have item
-        # probably need a display for searched
-        #item or place them in first in address book
-        # possibly a sort with item in it
 
     def printPostalE(self):
         first_index = self.list_box.curselection()[0]
         value = self.address[int(first_index)]
-#
         entry_to_print = self.to_entry(value)
         index = self.book.get_entry_index(entry_to_print)
         postal = self.book.get_entry(index).to_postal()
@@ -326,14 +279,7 @@ class MainWindow(object):
         self.top = tk.Toplevel(self.parent)
         for line in postal:
             tk.Label(self.top, text=line).pack(padx=20, pady=10)
-
-#
         print "print postal"
-        # do the postal rewriting and make it a string
-        #self.top = tk.Toplevel(self.parent)
-        #tk.Label(self.top, text="Postal line 1").pack(padx=20, pady=10)
-        #tk.Label(self.top, text="Postal line 2").pack(padx=20, pady=10)
-        #tk.Label(self.top, text="Postal line 3").pack(padx=20, pady=10)
 
     def sortbyE(self):
         selection_box = dialogs.PickAttribute(self.parent, self.book)
@@ -343,16 +289,12 @@ class MainWindow(object):
             self.update_list()
 
     def to_entry(self,value):
-        '''
-        '''
         attrs = []
         for i in range(len(self.book.get_fields())):
             attrs.append(value.split('\t')[i].split(":")[1])
         return entry.Entry(*attrs)
 
     def update_list(self):
-        '''
-        '''
         self.list_box.delete(0,tk.END)
         for item in self.address:
             self.list_box.insert(tk.END, item)
