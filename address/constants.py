b15fdc84e95fad6ba254c854a4ba90eea5d4a294
KirstenD <kdawes@uoregon.edu>
2015-01-29 19:31:32 -0800

    Update constants.py

--- address/constants.py
+++ address/constants.py
@@ -4,14 +4,14 @@ make it easier to use the name of the attributes of Entry
 and the key name in json file(those strings need to follow
 variable naming conventions,e.g. no space in between)
 """
-LNAME = "lname"
-FNAME = "fname"
-ADDR = "addr"
-CITY = "city"
-STATE = "state"
-ZIP_CODE = "zip_code"
-PHONE_NUM = "phone_num"
-EMAIL = "email"
+LNAME = "Last Name"
+FNAME = "First Name"
+ADDR = "Address"
+CITY = "City"
+STATE = "State"
+ZIP_CODE = "Zipcode"
+PHONE_NUM = "Phone Number"
+EMAIL = "Email"
 
 #for actions
 NEW = 1
