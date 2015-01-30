a67555b1bea61c82d57fd12aae0c15f0a765dfed
Skyler Berg <skylertheberg@gmail.com>
2015-01-29 16:03:04 -0800

    Make phone numbers that match ^\d{3}-\d{4}$ valid

--- address/utility.py
+++ address/utility.py
@@ -52,7 +52,7 @@ def validate(attr, string):  # return value may need to be improved
     if attr == EMAIL:
         return re.match(r"^\s*$|^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.\w+$", string)
     elif attr == PHONE_NUM:
-        return re.match(r"^\s*$|^\d{10}$|^\d{7}$|^\d{3}-\d{3}-\d{4}$|^\(\d{3}\)\d{3}-\d{4}$", string)
+        return re.match(r"^\s*$|^\d{10}$|^\d{7}$|^\d{3}-\d{3}-\d{4}$|^\(\d{3}\)\d{3}-\d{4}$|^\d{3}-\d{4}$", string)
     elif attr == ADDR:
         return re.match(r"^\s*$|^\d{1,6}( +\w+){2,3}( +\w{2,4} \d{1,5})?$", string)
     elif attr == ZIP_CODE:
