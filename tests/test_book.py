5ad8637ef6b34320e9c84d97b57244d27d1536a0
morgan <uoozhang@gmail.com>
2015-01-29 22:25:11 -0800

    add test cases for book class

--- tests/test_book.py
+++ tests/test_book.py
@@ -63,3 +63,53 @@ class TestBook(unittest.TestCase):
         ret = book.search("ste")
         self.assertEquals(len(ret), 3)
 
+    def test_book_search_string_in_three_entries_with_field(self):
+        book = Book()
+        en1 = address.entry.Entry("Stephanie","Nichols","3018 Annamark Lane")
+        en2 = address.entry.Entry("stephanie","nichols","3018 annamark lane")
+        en3 = address.entry.Entry("Thomas","Meyer","3018 Annamark Lane STE 4")
+        book.add_entry(en1)
+        book.add_entry(en2)
+        book.add_entry(en3)
+        ret = book.search("ste",ADDR)
+        self.assertEquals(len(ret), 1)
+
+    def test_book_sort_with_two_entries(self):
+        en1 = address.entry.Entry("fnamea","lnameb")
+        en2 = address.entry.Entry("fnameb","lnamea")
+        book1 = Book()
+        book2 = Book()
+        book1.add_entry(en1)
+        book1.add_entry(en2)
+        book2.add_entry(en2)
+        book2.add_entry(en1)
+        book1.sort(FNAME)
+        book2.sort(FNAME)
+        self.assertEquals(book1.get_entry(0), book2.get_entry(0))
+
+    def test_book_get_index_of_entry(self):
+        book = Book()
+        en1 = address.entry.Entry("Stephanie","Nichols","3018 Annamark Lane")
+        en2 = address.entry.Entry("stephanie","nichols","3018 annamark lane")
+        en3 = address.entry.Entry("Thomas","Meyer","3018 Annamark Lane STE 4")
+        book.add_entry(en1)
+        book.add_entry(en2)
+        book.add_entry(en3)
+        en = address.entry.Entry("Stephanie","Nichols","3018 Annamark Lane")
+        index = book.get_entry_index(en)
+        self.assertEquals(index, 0)
+
+    def test_book_set_entries(self):
+        book = Book()
+        en1 = address.entry.Entry("Stephanie","Nichols","3018 Annamark Lane")
+        en2 = address.entry.Entry("stephanie","nichols","3018 annamark lane")
+        en3 = address.entry.Entry("Thomas","Meyer","3018 Annamark Lane STE 4")
+        ens = [en1,en2,en3]
+        book.set_entries(ens)
+        self.assertEquals(len(book), 3)
+
+    def test_book_add_field(self):
+        book = Book()
+        en1 = address.entry.Entry("Stephanie","Nichols","3018 Annamark Lane")
+        book.add_field("new_field")
+        self.assertEquals(len(book.get_fields()), 9)
