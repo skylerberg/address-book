0bdacf869f445b42cdc33a32a949710a5e9e1062
KirstenD <kdawes@uoregon.edu>
2015-01-30 19:51:26 -0800

    added doc string

--- address/gui/messagebox.py
+++ address/gui/messagebox.py
@@ -9,6 +9,13 @@ WARNING = 1
 ASK = 2
 
 def message(message_type, message, **kwargs ):
+    '''
+    Shows error to user, different types of errors, warnings, or asking user.
+    args: message types - ERROR, WARNING or ASK
+          message - string that is describing the error
+          **kwargs - what every is casuing that error. 
+    returns : tkMessageBox
+    '''
     if message_type == ERROR:
         return tkMessageBox.showerror("Error",message,**kwargs)
     elif message_type == WARNING:
